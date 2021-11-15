115384e443e04e910dbdc432b225f6ec2c5a4b1e
Yamin05114 <liyamin0514@gmail.com>
2021-11-15 16:42:13 +0800

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -34,9 +34,9 @@ class DETR(nn.Module):
         self.num_queries = num_queries
         self.transformer = transformer
         hidden_dim = transformer.d_model
-        self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
-        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
-        self.query_embed = nn.Embedding(num_queries, hidden_dim)
+        self.class_embed = nn.Linear(hidden_dim, num_classes + 1)  # class直接liear出
+        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)  # bbox需要MLP？？
+        self.query_embed = nn.Embedding(num_queries, hidden_dim)  
         self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
         self.backbone = backbone
         self.aux_loss = aux_loss
